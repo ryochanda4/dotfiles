0d395d92e7bb587858c49405f8c8e0788bc18211
Ryo Tsuyuki <ryochanda4@gmail.com>
2013-04-21 23:39:15 +0900

    modified setup.sh to create an alias

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 
 #!/bin/bash
 
+# set symbolic links 
 DOT_FILES=( .vimrc .vim .dir_colors )
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+# for bash and zsh, only if no alias is already set
+alias vi >/dev/null 2>&1 || alias vi=vim
