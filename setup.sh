e717e5c422ffed20ab7dddb8f9c4ef10fe5d6ded
r.tsuyuki <r.tsuyuki@SUVT000353-no-MacBook-Pro.local>
2015-04-23 10:52:31 +0900

    added .vimrc

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # change current directory
+# if you want to change vim settings, refer the following link
+# http://vim-bootstrap.com/
+
 HOME_DIR=`dirname $0`
 cd ${HOME_DIR}
 
-DOT_FILES=( .dir_colors .bash_profile .bashrc  )
+DOT_FILES=( .dir_colors .bash_profile .bashrc .vimrc )
 for f in ${DOT_FILES[@]}; do ln -s $HOME/dotfiles/$f $HOME/$f; done
-
-# vim setup
-curl https://raw.github.com/avelino/.vimrc/master/bootstrap.sh -o - | sh
 
